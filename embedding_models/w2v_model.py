d83a3a2692117bebcf249e32e1d4c0c8438a2417
Yui Zhou <yui.zhou97@gmail.com>
2022-08-13 22:51:56 -0400

    delete w2v models from git (upload manually to embedding_models/)

---
+++ embedding_models/w2v_model.py
@@ -0,0 +1,21 @@
+import os
+from gensim.models import KeyedVectors, Word2Vec
+
+def get_w2vmodel(dataset):
+    w2vmodel = None
+    abs_dirname = os.path.dirname(os.path.abspath(__file__))
+
+    if dataset == 'covid_tweets':
+        coronavirus_CP_path = '/corpusgood_sinrepeticiones.w2v'
+        w2vmodel = Word2Vec.load(abs_dirname + coronavirus_CP_path)
+        return w2vmodel
+
+    elif dataset == 'hurricane_tweets':
+        hurricane_path = '/huracanes_sinrepeticiones.w2v'
+        w2vmodel = Word2Vec.load(abs_dirname + hurricane_path)
+        return w2vmodel
+
+    elif dataset == 'wikidump':
+        wikidump_path = '/wiki_dump_s200_w5_c5_e30.kv'
+        w2vmodel = KeyedVectors.load(abs_dirname + wikidump_path)
+        return w2vmodel
