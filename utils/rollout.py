5b0b954b28637a8903de2c0b21b6339d5ec04200
Yui Zhou <yui.zhou97@gmail.com>
2022-08-15 00:15:43 -0400

    remove other GANs calls

--- utils/rollout.py
+++ utils/rollout.py
@@ -62,76 +62,6 @@ class ROLLOUT:
 
         return samples
 
-    def rollout_mc_search_leakgan(self, sentences, dis, given_num):
-
-        batch_size, seq_len = sentences.size()
-
-        goal_array = torch.zeros((batch_size, seq_len + 1, self.goal_out_size))
-
-        work_hidden = self.gen.init_hidden(batch_size)
-        mana_hidden = self.gen.init_hidden(batch_size)
-        real_goal = self.gen.goal_init[:batch_size, :]
-        out = 0
-
-        if self.gpu:
-            goal_array = goal_array.cuda()
-            real_goal = real_goal.cuda()
-
-        # get current state
-        for i in range(given_num):
-            # Get feature.
-            dis_inp = torch.zeros(batch_size, seq_len).long()
-            dis_inp[:, :i + 1] = sentences[:, :i + 1]  # cut sentences
-            leak_inp = sentences[:, i]
-            if self.gpu:
-                dis_inp = dis_inp.cuda()
-                leak_inp = leak_inp.cuda()
-            feature = dis.get_feature(dis_inp).unsqueeze(0)
-
-            # Get output of one token
-            # cur_goal: batch_size * 1 * goal_out_size
-            out, cur_goal, work_hidden, mana_hidden = self.gen(i, leak_inp, work_hidden, mana_hidden,
-                                                               feature, real_goal, train=True)
-
-            # Save goal and update last_goal
-            goal_array[:, i, :] = cur_goal.squeeze(1)
-            if i > 0 and i % self.step_size == 0:
-                real_goal = torch.sum(goal_array[:, i - 3:i + 1, :], dim=1)
-                if i / self.step_size == 1:
-                    real_goal += self.gen.goal_init[:batch_size, :]
-
-        samples = torch.zeros(batch_size, self.max_seq_len).long()
-        samples[:, :given_num] = sentences[:, :given_num]
-
-        # MC search
-        for i in range(given_num, self.max_seq_len):
-            # Sample one token
-            out = torch.multinomial(torch.exp(out), 1).view(-1)  # [num_samples] (sampling from each row)
-            samples[:, i] = out.data
-
-            # Get feature
-            dis_inp = samples
-            if self.gpu:
-                dis_inp = dis_inp.cuda()
-            feature = dis.get_feature(dis_inp).unsqueeze(0)
-            leak_inp = out
-
-            # Get output of one token
-            # cur_goal: batch_size * 1 * goal_out_size
-            out, cur_goal, work_hidden, mana_hidden = self.gen(i, leak_inp, work_hidden, mana_hidden,
-                                                               feature, real_goal, train=True)
-
-            # Save goal and update last_goal
-            goal_array[:, i, :] = cur_goal.squeeze(1)
-            if i > 0 and i % self.step_size == 0:
-                real_goal = torch.sum(goal_array[:, i - 3:i + 1, :], dim=1)
-                if i / self.step_size == 1:
-                    real_goal += self.gen.goal_init[:batch_size, :]
-
-        if self.gpu:
-            samples = samples.cuda()
-
-        return samples
 
     def get_reward(self, sentences, rollout_num, dis, current_k=0):
         """
@@ -160,6 +90,7 @@ class ROLLOUT:
         # rewards = torch.mean(rewards, dim=0)
         rewards = torch.mean(rewards.view(batch_size, self.max_seq_len, rollout_num), dim=-1)
         return rewards
+
 
     def get_reward_similarity(self, sentences, rollout_num):
 
@@ -218,36 +149,6 @@ class ROLLOUT:
         rewards = torch.mean(rewards.view(batch_size, self.max_seq_len, rollout_num), dim=-1)
         return rewards
 
-
-    def get_reward_leakgan(self, sentences, rollout_num, dis, current_k):
-        """
-        get reward via Monte Carlo search for LeakGAN
-        :param sentences: size of batch_size * max_seq_len
-        :param rollout_num:
-        :param dis:
-        :param current_k: current training gen
-
-        :return: reward: batch_size * (max_seq_len / step_size)
-        """
-        with torch.no_grad():
-            batch_size = sentences.size(0)
-            rewards = torch.zeros([rollout_num * (self.max_seq_len // self.step_size), batch_size]).float()
-            if self.gpu:
-                rewards = rewards.cuda()
-            idx = 0
-            for i in range(rollout_num):
-                for t in range(self.max_seq_len // self.step_size):
-                    given_num = t * self.step_size + 1  # 1, 5, 9, ..
-                    samples = self.rollout_mc_search_leakgan(sentences, dis, given_num)
-                    out = dis(samples)
-                    out = F.softmax(out, dim=-1)
-                    reward = out[:, current_k + 1]
-                    rewards[idx] = reward
-                    idx += 1
-
-        rewards = rewards.view(batch_size, self.max_seq_len // self.step_size, rollout_num)
-        rewards = torch.mean(rewards, dim=-1)
-        return rewards
 
     def get_token_reward(self, sentences, rollout_num, dis, current_k, given_num):
         """
